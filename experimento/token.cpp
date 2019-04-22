fd54d2583b1d6a83ab899e0069e76b4632a22bce
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-04-22 15:15:40 -0600

    Cambios token

--- experimento/token.cpp
+++ experimento/token.cpp
@@ -10,14 +10,14 @@ int main() {
 	vector<int>numero;
 	//numero.push_back(1);
 	//numero.push_back(2);
-	//numero.push_back(12);
+	numero.push_back(12);
 	//numero.push_back(18);
 	//numero.push_back(24);
 
 	vector<string>valor;
 	//valor.push_back("4");
 	//valor.push_back("8");
-	//valor.push_back("91");
+	valor.push_back("91");
 	//valor.push_back("14");
 	//valor.push_back("15");
 	p.ejecutar("Ventana",numero,valor);
